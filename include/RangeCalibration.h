546325cbfe6c6c53ed1310fac1923043314023cc
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2025-10-08 13:44:14 -0400

    calibration update

--- include/RangeCalibration.h
+++ include/RangeCalibration.h
@@ -9,10 +9,23 @@ class RangeCalibration {
     float compensateUltrasonicDistance(float rawDistance, float temperature, float humidity, float pressure);
 
     private:
-    const float speed_of_sound_default = 331.45;  // m/s
+    const float speed_of_sound_default = 343.5;  // m/s
     const float temperature_default = 20.0;  // C
     const float humidity_default = 0.0;  // %
-    const float pressure_default = 101.325;   // kPa
+    const float pressure_default = 100;   // kPa
+
+    // Model 1
+    const float a_t = 1.779762;  // m/s/°C
+    const float a_rh = -20.035801; // m/s
+    const float a_p = 0.0; // m/s/kPa
+
+    // Model 3
+    const float a_0 = -77.576344;  // mm
+    const float a_1 = 0.000266;  // mm/ns
+    const float b_0 = -0.000003;  // mm/(ns·°C)
+    const float b_1 = -0.000136;  // mm/ns
+    const float b_2 = 0.0;  // mm/(ns·kPa)
+    const float b_3 = 6.535015;  // mm/°C
 
 };
 
--- src/RangeCalibration.cpp
+++ src/RangeCalibration.cpp
@@ -1,5 +1,9 @@
 #include "RangeCalibration.h"
 
+#define USE_DISTANCE_MODEL true
+// #define USE_MODEL_1 1
+
+
 // Function to calculate speed of sound based on T/RH/P
 float RangeCalibration::calculateSpeedOfSound(float temperature, float humidity, float pressure) {
     // Calculate speed of sound in mm/s
@@ -9,9 +13,9 @@ float RangeCalibration::calculateSpeedOfSound(float temperature, float humidity,
     // float speedOfSound = (speed_of_sound_default + 0.606 * temperature) * 1000; // mm/s
 
     float speedOfSound = speed_of_sound_default;
-    speedOfSound += a_t * (temperature - temperature_default);
-    speedOfSound += a_rh * (humidity - humidity_default);
-    speedOfSound += a_p * (pressure - pressure_default);
+    speedOfSound += (a_t * (temperature - temperature_default));
+    speedOfSound += (a_rh * (humidity - humidity_default));
+    speedOfSound += (a_p * (pressure - pressure_default));
     speedOfSound *= 1000;
 
     return speedOfSound;  // return mm/s
@@ -21,12 +25,25 @@ float RangeCalibration::calculateSpeedOfSound(float temperature, float humidity,
 float RangeCalibration::compensateUltrasonicDistance(float rawDistance, float temperature, float humidity, float pressure) {
     // Convert the raw distance back to time of flight
     float timeOfFlight_ns = (rawDistance * 2.0) / 343500.0 * 1e9; // Convert to nanoseconds
+    float delta_t = temperature - temperature_default;
+    float delta_rh = humidity - humidity_default;
+    float delta_p = pressure - pressure_default;
 
     // Calculate actual speed of sound
     float actualSpeed = calculateSpeedOfSound(temperature, humidity, pressure);
 
     // Calculate compensated distance using actual speed of sound
-    float compensatedDistance = (timeOfFlight_ns * 1e-9) * actualSpeed / 2.0;
+    float compensatedDistance = 0;
+    if (!USE_DISTANCE_MODEL) {
+        compensatedDistance = (timeOfFlight_ns * 1e-9) * actualSpeed / 2.0;
+    }
+    if (USE_DISTANCE_MODEL) {
+        compensatedDistance = a_0 + a_1 * timeOfFlight_ns;
+        compensatedDistance += b_0 * delta_t * timeOfFlight_ns;
+        compensatedDistance += b_1 * delta_rh * timeOfFlight_ns;
+        compensatedDistance += b_2 * delta_p * timeOfFlight_ns;
+        compensatedDistance += b_3 * delta_t;
+    }
 
     return compensatedDistance; // return mm
 }
