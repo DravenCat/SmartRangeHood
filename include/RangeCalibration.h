3c477da215ce75c458c7ef74d007ee4fe7c02599
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2025-10-08 16:34:31 -0400

    calibration update

--- include/RangeCalibration.h
+++ include/RangeCalibration.h
@@ -1,6 +1,7 @@
 #ifndef RSCALIBRATION_RANGECALIBRATION_H
 #define RSCALIBRATION_RANGECALIBRATION_H
 
+// #define USE_MODEL_4
 
 class RangeCalibration {
 
@@ -12,13 +13,21 @@ class RangeCalibration {
     const float speed_of_sound_default = 343.5;  // m/s
     const float temperature_default = 20.0;  // C
     const float humidity_default = 0.0;  // %
+#ifndef USE_MODEL_4
     const float pressure_default = 100;   // kPa
+#endif
+
+#ifdef USE_MODEL_4
+    const float pressure_default = 101;   // kPa
+#endif
+
 
     // Model 1
     const float a_t = 1.779762;  // m/s/°C
     const float a_rh = -20.035801; // m/s
     const float a_p = 0.0; // m/s/kPa
 
+#ifndef USE_MODEL_4
     // Model 3
     const float a_0 = -77.576344;  // mm
     const float a_1 = 0.000266;  // mm/ns
@@ -26,6 +35,18 @@ class RangeCalibration {
     const float b_1 = -0.000136;  // mm/ns
     const float b_2 = 0.0;  // mm/(ns·kPa)
     const float b_3 = 6.535015;  // mm/°C
+
+#endif
+
+#ifdef USE_MODEL_4
+    // Model 4
+    const float a_0 = -78.312327;  // mm
+    const float a_1 = 0.000130;  // mm/ns
+    const float b_0 = -0.000001;  // mm/(ns·°C)
+    const float b_1 = 0.000150;  // mm/ns
+    const float b_2 = 0.000006;  // mm/(ns·kPa)
+    const float b_3 = 6.480966;  // mm/°C
+#endif
 
 };
 
